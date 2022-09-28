5c44f35a37214e28d0ff68dd52e5a918782ed924
twatson5543 <60808337+twatson5543@users.noreply.github.com>
2022-09-28 08:16:14 -0400

    Add files via upload
    20220928-081610

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# //// Imports
+import os
+
+# ========== Global SRT ==========
+
+class G1:  # .. Global Functions, used in every program by default at start of program
+    def SystemOS():  # .... Determines user Operating System, returns value ... | 0. Could not find OS | 1. Windows OS | 2. Linux OS | 3. Other OS |
+        pass
+
+# ========== Global END ==========
+
+
+# ========== DEBUG SRT ==========
+
+# ========== DEBUG END ==========
+
+
+# ========== Functions SRT ========== ** Class Functions for Project
+
+# //// A1 SRT
+class A1:  # .. Example Class
+    def Func1():  # .... Example Function
+        pass
+# //// A1 END
+
+# ========== Functions END ==========
